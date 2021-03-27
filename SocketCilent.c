03cdbcb23f635e2ed5c847d47089e270c94ef517
pj <525967603@qq.com>
2021-03-27 23:12:58 +0800

    添加README文件

--- SocketCilent.c
+++ SocketCilent.c
@@ -7,34 +7,46 @@
 #include<string.h> 
 #include<unistd.h>
 
-#define SOCK_PORT 8538  //端口预设
+//固定端口
+#define SOCK_PORT 8538  
 #define BUFFER_LEN 2048 
 
+//增加main函数参数用于手动输入端口
 int main(int argc,char* argv[])
 {
 	int sockfd;
 	int tempfd;
 	struct sockaddr_in s_addr_in;
-	char send_data[BUFFER_LEN]; //发送的数据
-	char recv_data[BUFFER_LEN]; //接收的数据
-	//初始化数组
+	char send_data[BUFFER_LEN]; 
+	char recv_data[BUFFER_LEN]; 
+	
+	//初始化发送和接收数据的数组
 	memset(send_data,0,BUFFER_LEN);
 	memset(recv_data,0,BUFFER_LEN);
 	
-	sockfd = socket(AF_INET,SOCK_STREAM,0);  //ipv4,tcp
+	sockfd = socket(AF_INET,SOCK_STREAM,0);
 	if(sockfd == -1)
 	{
 	fprintf(stderr, "[%s:%d] socket error!\n", __FILE__, __LINE__);
 	exit(1);
 	}
 
+
 	memset(&s_addr_in,0,sizeof(s_addr_in));
-	s_addr_in.sin_addr.s_addr = inet_addr("127.0.0.1"); //inet_addr将一个点间隔地址转换成网络字节序	
+	
+	//设定IP地址
+	s_addr_in.sin_addr.s_addr = inet_addr("127.0.0.1");
+	//设定为IPv4
 	s_addr_in.sin_family = AF_INET;
+	//设定为固定端口号
 	//s_addr_in.sin_port = htons(SOCK_PORT);
+	
+	//设定为动态端口
 	int port = atoi(argv[1]);
 	s_addr_in.sin_port = htons(port);
+	
 
+	//向服务端发起连接请求
 	tempfd = connect(sockfd,(struct sockaddr*) &s_addr_in,sizeof(s_addr_in));
 	if(tempfd == -1)
 	{
@@ -45,8 +57,10 @@ int main(int argc,char* argv[])
 	while(1)
 	{
 		printf("please input anything you wanna tell server.....(input \"quit\" to quit)\n");
-		scanf("%[^\n]",send_data);
+		scanf("%[\n]",send_data);
 		getchar();
+
+		//将信息发送给服务端
 		tempfd = write(sockfd,send_data,BUFFER_LEN);
 		
 		if(tempfd == -1)
@@ -61,12 +75,17 @@ int main(int argc,char* argv[])
 		}
 		else
 		{
+
+		//接收来自服务端信息并输出
 		tempfd = read(sockfd,recv_data,BUFFER_LEN);
 		assert(tempfd != -1);
 		printf("%s\n",recv_data);
+
+		//清空接收和发送数据的数组
 		memset(send_data,0,BUFFER_LEN);
 		memset(recv_data,0,BUFFER_LEN);
 		}
 	}
+
 	close(sockfd);
 }
--- SocketServer.c
+++ SocketServer.c
@@ -9,10 +9,12 @@
 #include<unistd.h>
 #include<time.h>
 
-#define SOCK_PORT 8538
+//固定端口(specified port)
+//#define SOCK_PORT 8538
 #define MAX_CONN_LIMIT 10
 #define BUFFER_LEN 2048
 
+//增加main函数参数用于手动输入端口
 int main(int argc,char* argv[])
 {
 
@@ -27,31 +29,40 @@ int main(int argc,char* argv[])
 	char recv_data[BUFFER_LEN];
 	char send_data[BUFFER_LEN];
 	
-	//增加回应系统时间功能
+	//增加回应系统时间的功能(add function for reply system time)
 	time_t timep;
 	time (&timep);
 	char tmp[2048];
-	//strftime(tmp,sizeof(tmp),"%Y-%m-%d %H:%M:%S",localtime(&timep));
-
-	sockfd_server = socket(AF_INET,SOCK_STREAM,0); //使用ipv4，TCP
-	assert(sockfd_server != -1);  //判断是否返回成功
 	
+	//使用ipv4、TCP
+	sockfd_server = socket(AF_INET,SOCK_STREAM,0);
 	
-	memset(&s_addr_in,0,sizeof(s_addr_in));  //初始化
-	s_addr_in.sin_family = AF_INET; //设置为ipv4
-	s_addr_in.sin_addr.s_addr = htonl(INADDR_ANY); //储存任意ip地址 
-	//s_addr_in.sin_port = htons(SOCK_PORT); //设置port
+	//判断是否返回成功
+	assert(sockfd_server != -1); 
+       	
+	memset(&s_addr_in,0,sizeof(s_addr_in));
+	
+	//设置为ipv4
+	s_addr_in.sin_family = AF_INET;
+	//设置IP地址
+	s_addr_in.sin_addr.s_addr = htonl(INADDR_ANY); 
+	//使用固定端口
+	//s_addr_in.sin_port = htons(SOCK_PORT);
+	
+	//动态指定端口
 	int port=atoi(argv[1]);
-	s_addr_in.sin_port = htons(port); //输入端口绑定
+	s_addr_in.sin_port = htons(port);
 
-	fd_temp = bind(sockfd_server,(void*)(&s_addr_in),sizeof(s_addr_in));  //绑定地址和端口
+	//绑定地址和端口
+	fd_temp = bind(sockfd_server,(void*)(&s_addr_in),sizeof(s_addr_in));
 	if(fd_temp == -1)
 	{
 		fprintf(stderr, "[%s:%d] bind error!\n", __FILE__, __LINE__);
 		exit(1);
 	}
 
-	fd_temp = listen(sockfd_server,MAX_CONN_LIMIT);  //监听此socket
+	//监听此socket，等待客户端的连接请求
+	fd_temp = listen(sockfd_server,MAX_CONN_LIMIT);
 	if(fd_temp == -1)
 	{
 		fprintf(stderr, "[%s:%d] listen error!\n", __FILE__, __LINE__);
@@ -61,21 +72,28 @@ int main(int argc,char* argv[])
 
 	printf("waiting a new connect.....\n");
 	cilent_length = sizeof(s_addr_cilent);
+	
+	//接收请求，并完成连接
 	sockfd = accept(sockfd_server,(void*) &s_addr_cilent,(socklen_t*) &cilent_length);
+	
 	while(1)
 	{
 		if(sockfd == -1)
 		{
-		fprintf(stderr, "[%s:%d] accept error!\n", __FILE__, __LINE__);
-		continue;
+			fprintf(stderr, "[%s:%d] accept error!\n", __FILE__, __LINE__);
+			continue;
 		}
 	
 		printf("waiting a msg....\n");
 		
-		memset(recv_data,0,BUFFER_LEN); //初始化数组
+		//清空接收、发送和时间的数组
+		memset(recv_data,0,BUFFER_LEN); 
 		memset(send_data,0,BUFFER_LEN);
 		memset(tmp,0,sizeof(tmp));
-		i_recv = read(sockfd,recv_data,BUFFER_LEN); //接受cilent信息
+
+		//接收来自客户端的消息,并对内容进行判断
+		i_recv = read(sockfd,recv_data,BUFFER_LEN);
+		
 		if(i_recv == 0)
 		{
 			printf("cilent maybe close\n");
@@ -91,16 +109,24 @@ int main(int argc,char* argv[])
 			printf("cilent quit!\n");
 			break;
 		}
+
+		//获取系统时间
 		strftime(tmp,sizeof(tmp),"%Y-%m-%d %H:%M:%S :",localtime(&timep));
+		
 		//测试是否每次均成功获取时间
 		//printf("%s\n",tmp);
+		
 		printf("cilent : %s \n",recv_data);
+		
+		//将返回的客户端消息加上时间
 		strcpy(send_data,recv_data);
 		strcat(tmp,send_data);
-		printf("%s\n",tmp);
 		write(sockfd,tmp,sizeof(tmp));
+		
+		//原样返回客户端消息
 		//write(sockfd,send_data,BUFFER_LEN);
 	}
+
 	close(sockfd);
 	close(sockfd_server);
 }
